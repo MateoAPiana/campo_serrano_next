e6e2b69f1f4a578887326f60493772ce35df5e58
mateo <mateoapiana@gmail.com>
2025-04-15 12:19:24 -0300

    walks

---
+++ src/app/products/services/walks/page.tsx
@@ -0,0 +1,24 @@
+import "./walks.css";
+import { productsWalks } from "./productsWalks";
+
+
+export default function Walks() {
+  return (
+    <main className="walksPage">
+      {
+        productsWalks.map((p, index) => {
+          return (
+            <a
+              key={index}
+              href={`/products/service/walks/descriptions/${p}`}
+              style={{ "backgroundImage": `url('/service/${p}.png')` }}
+              className="walks__item"
+            >
+              <h2>{p}</h2>
+            </a>
+          );
+        })
+      }
+    </main>
+  )
+}

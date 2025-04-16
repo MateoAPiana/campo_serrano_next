da5fec1a5a4a232ef4be9c85f4d8a1837ba8c812
mateo <mateoapiana@gmail.com>
2025-04-16 17:36:19 -0300

    footer path icons

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -4,11 +4,11 @@ export function Contact() {
   return (
     <section id="contact" className="contact">
       <div className="email">
-        <img src="/favicon-email.png" alt="" />
+        <img src="/assets/favicon-email.png" alt="" />
         <p className="info">example@gmail.com</p>
       </div>
       <div className="instagram">
-        <img src="/favicon-instagram.png" alt="" />
+        <img src="/assets/favicon-instagram.png" alt="" />
         <p className="info">example@gmail.com</p>
       </div>
     </section>

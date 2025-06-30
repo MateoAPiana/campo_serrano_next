31dcbb93c8610f83fac32a9b2b9143860e4b7f7a
mateo <mateoapiana@gmail.com>
2025-06-30 10:18:30 -0300

    home arrow

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -14,22 +14,22 @@ export default async function Home({
     <main className="main_home">
       <div className="video__wrapper">
         <video id="videoHome" loop src="/assets/videomock.mp4"></video>
+        <a href="#about" className="arrow">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"></path>
+          </svg>
+        </a>
       </div>
-      <a href="#about" className="arrow">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="size-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"></path>
-        </svg>
-      </a>
       <section className="about" id="about">
         <div className="fringe"></div>
         <div className="content_about">

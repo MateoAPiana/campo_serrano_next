3d765075d1b8955c85bbfa826e7c5779830fcbfd
mateo <mateoapiana@gmail.com>
2025-08-08 10:21:06 -0300

    reservation selected page

--- src/app/[lang]/reservation/page.tsx
+++ src/app/[lang]/reservation/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./styles.css"
 
 import { getTranslations } from "app/app/translations/translate";
@@ -14,7 +15,14 @@ export default async function Reservation({
 
   return (
     <main className="reservation_page">
-      <h1>{t && t?.("navBar", "Reservation")}</h1>
+      <h1 className="reservation_page__title">{t && t?.("navBar", "Reservation")}</h1>
+      <div className="reservation__container">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae doloribus reprehenderit quibusdam. Sint quos dolorem quibusdam? Minus eligendi, inventore, quas consequatur, vel modi qui earum quam suscipit deleniti sit laudantium.
+      </div>
+      <div className="links_wrapper">
+        <Link href={`/${lang}/reservation/gastronomy`} className="reservation__item reservation__gastronomy">{t && t?.("reservation", "title")}</Link>
+        <Link href={`/${lang}/reservation/events`} className="reservation__item reservation__events">{t && t?.("reservation_events", "title")}</Link>
+      </div>
     </main>
   )
 }
